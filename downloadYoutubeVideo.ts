3404b9b43d6c669e8c2592b683a6c8f06cab2164
Again <godshades6@gmail.com>
2025-01-28 14:55:31 +0700

    update

--- downloadYoutubeVideo.ts
+++ downloadYoutubeVideo.ts
@@ -17,10 +17,11 @@ export async function downloadYoutubeVideo(url: string, videoName: string) {
   const downloadPath = path.resolve(__dirname, `videos/${videoName}.mp4`);
 
   console.log(`Start downloading video ${url}`);
+  const proxyGermany = 'socks5://nyRg8C2D:irqski6G@154.196.77.86:64269';
 
   try {
     const { stdout } = await execPromise(
-      `yt-dlp -f "bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720]" \
+      `yt-dlp --proxy "${proxyGermany}" --cookies-from-browser firefox -f "bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720]" \
       --merge-output-format mp4 \
       -4 ${url} -o "${downloadPath}"`
     );
@@ -35,7 +36,7 @@ export async function downloadYoutubeVideo(url: string, videoName: string) {
   }
 }
 
-// const url = 'https://www.youtube.com/shorts/a0k6LHpSmWs';
-// downloadYoutubeVideo(url, 'Здесь веселок');
+const url = 'https://www.youtube.com/shorts/3nfVRiruvd4';
+downloadYoutubeVideo(url, 'Здесь веселок');
 
 // const videoId = url.split('/').at(-1)
